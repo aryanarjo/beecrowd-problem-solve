b927f69bcf3e78b2dd413c6cbdc9b1df7256d543
Aryan Saha <aryansaha1119@gmail.com>
2026-01-12 11:57:18 +0600

    Create 1002 - Area of a Circle.cpp

---
+++ 1002 - Area of a Circle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    double R,A;
+    const double n = 3.14159;
+    cin >> R;
+    A = n* R * R;
+    cout << fixed << setprecision(4);
+    cout << "A=" << A << endl;
+    return 0;
+}
